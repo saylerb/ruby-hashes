542c9de8fae27853d49942f87704060fe4b22bfd
Brian Sayler <sayler.b@gmail.com>
2017-01-24 23:54:00 -0700

    add array vs. hash comparison

--- array_versus_hash.rb
+++ array_versus_hash.rb
@@ -4,11 +4,17 @@
 
 require 'benchmark'
 
-names = ['Yehuda', 'David', 'Matz', 'Sandi']
+GC.disable
+#names = ['Yehuda', 'David', 'Matz', 'Sandi']
+names = ['Yehuda', 'David', 'Matz', 'Sandi', '5', '6', '7', '8', '9', '10', '11', '12', '13']
 
-grades = ['A', 'B-', 'A+', 'B+']
+#twitter_handle = ['wycats', 'dhh', 'yukihiro_matz', 'sandimetz']
+twitter_handle = ['wycats', 'dhh', 'yukihiro_matz', 'sandimetz', '5', '6', '7', '8', '9', '10', '11', '12', '13']
 
-projects = ['Sorting Suite', 'Black Thursday', 'Little Shop', 'The Pivot']
+followers = [58_679, 192_471, 45_840, 20_400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300]
+
+locations = ['Portland', 'Chicago', 'Matsue', 'Durham', 'NYC', 'Dallas', 'San Fransisco', 'Pune', 'Paris', 'London', 'Seattle', 'Miami', 'Charlotte']
+#locations = ['Portland', 'Chicago', 'Matsue', 'Durham']
 
 # Now, what if we wanted to draw connections between these pieces of data?
 # Information is stored across lists at a common index (e.g. Max is at the 3rd index)
@@ -22,19 +28,18 @@ projects = ['Sorting Suite', 'Black Thursday', 'Little Shop', 'The Pivot']
 
 # Lists need to be
 
-def get_grade(student, name_list, grade_list, project_list)
-  index = name_list.index(student) # => returns the index number
-  grade = grade_list[index]
-  project = project_list[index]
+def get_data(name, names_list, followers_list, locations_list)
+  index = names_list.index(name) # => returns the index number
+  followers = followers_list[index]
+  location = locations_list[index]
 
-  return [project, grade]
+  return [followers, location]
 end
 
-#puts get_grade('Ana', names, grades, projects).inspect
-#puts get_grade('John', names, grades, projects).inspect
-#puts get_grade('Max', names, grades, projects).inspect
-#puts get_grade('Kay', names, grades, projects).inspect
-
+#puts get_data('Yehuda', names, followers, locations).inspect
+#puts get_data('David', names, followers, locations).inspect
+#puts get_data('Matz', names, followers, locations).inspect
+#puts get_data('Sandi', names, followers, locations).inspect
 
 # Solution: using a Hash
 
@@ -44,29 +49,44 @@ end
 
 # TODO: Lookups
 
-grade_hash = { 'Yehuda' => 4, 'David' => 3, 'Matz' => 4, 'Sandi' => 2 }
+#followers_hash = { 'Yehuda' => 58_679, 'David' => 192_471, 'Matz' => 45_840, 'Sandi' => 20_400 }
+#followers_hash = { 'Yehuda' => 58_679, 'David' => 192_471, 'Matz' => 45_840, 'Sandi' => 20_400, 'Brian' => 20_000, 'Erin' => 20_000, 'Nick' => 20_000, 'Jeff' => 1_000_000, 'Josh' => 1, 'Meeka' => 100, 'what' => 100, 'wowow' => 10_000, 'hello' => 1_000 }
 
+followers_hash = { :Yehuda => 58_679, :David => 192_471, :Matz => 45_840, :Sandi => 20_400, :Brian => 20_000, :Erin => 20_000, :Nick => 20_000, :Jeff => 1_000_000, :Josh => 1, :Meeka => 100, :what => 100, :wowow => 10_000, :hello => 1_000 }
+
+#
 # Benchmarking
 
-Benchmark.bm(10) do |x|
-  x.report("Array indexing:") { get_grade('Sandi', names, grades, projects) }
-end
+#Benchmark.bm(10) do |x|
+#  x.report("Array indexing:") { get_data('Sandi', names, followers, locations) }
+#end
 
+
+puts get_data('12', names, followers, locations).inspect
 method_time = Benchmark.realtime do
-  get_grade('Sandi', names, grades, projects)
+  1_000_000.times do
+    get_data('13', names, followers, locations)
+  end
 end
 
+puts followers[12].inspect
 array_time = Benchmark.realtime do
-  grades[4]
+  1_000_000.times do
+    followers[12]
+  end
 end
 
+puts followers_hash[:hello].inspect
 hash_time = Benchmark.realtime do
-  grade_hash['Sandi']
+  1_000_000.times do
+    #followers_hash[:hello]
+    followers_hash.dig(:hello)
+  end
 end
 
+GC.enable
 
 puts "Method time elapsed #{method_time * 1000} milliseconds"
 puts "Array time elapsed #{array_time * 1000} milliseconds"
 puts "Hash time elapsed #{hash_time * 1000} milliseconds"
-
 
